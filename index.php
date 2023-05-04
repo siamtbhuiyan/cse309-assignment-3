0277ba1b4f321a107439d07d2839b1b7725e9f4b
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2023-05-04 15:46:07 +0600

    Data Formated

--- index.php
+++ index.php
@@ -1,7 +1,55 @@
 <?php
+    class Weather {
+        public $date;
+        public $temp;
+        public $name;
+        public $description;
+        public $icon;
+
+        function set_date($date) {
+            $this->date = $date;
+        }
+        function set_temp($temp) {
+            $this->temp = round($temp-273.15);
+        }
+        function set_name($name) {
+            $this->name = $name;
+        }
+        function set_description($description) {
+            $this->description = $description;
+        }
+        function set_icon($icon) {
+            $this->icon = "https://openweathermap.org/img/wn/$icon@2x.png";
+        }
+    }
+
+    $forecast = array();
+
     $url = "https://api.openweathermap.org/data/2.5/forecast?lat=23.710&lon=90.407&appid=c908b1113a2c788367ba7b7e051be74e";
     $content = file_get_contents($url);
     $clima = json_decode($content);
+
+    $list = $clima->list;
+
+    $dateTest = "";
+    
+
+    foreach ($list as $weather) {
+        $dateTemp = explode(" ", trim($weather->dt_txt))[0];
+        if ($dateTemp != $dateTest) {
+            
+            $currentWeather = new Weather ();
+            $currentWeather->set_date(date("d M, Y", $weather->dt));
+            $currentWeather->set_temp($weather->main->temp); 
+            $currentWeather->set_name($weather->weather[0]->main); 
+            $currentWeather->set_description($weather->weather[0]->description); 
+            $currentWeather->set_icon($weather->weather[0]->icon); 
+
+            array_push($forecast, $currentWeather);
+
+            $dateTest = $dateTemp;
+        }
+    }
 ?>
 
 <!DOCTYPE html>
